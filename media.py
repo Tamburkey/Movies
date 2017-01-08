d1c3a170c81425b4214c7b16fc9485e71e66b8f8
tamburkey <petem14@gmail.com>
2017-01-07 19:09:04 -0500

    docstrings added

--- media.py
+++ media.py
@@ -2,11 +2,13 @@ import webbrowser
 
 class Movie():
 	"""class with instance variables describing the movie"""
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, release_date):  # Constructor w Instance Variables
+	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, release_date):  
+		""" Constructor for class Movie w Instance Variables """
 		self.title = movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 		self.release_date = release_date
-	def show_trailer(self):  # Instance Method of class Movie
+	def show_trailer(self):  
+		""" Instance Method of class Movie which opens youtube """
 		webbrowser.open(self.trailer_youtube_url)
